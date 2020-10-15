6fe93cc5b0b80799010b278a96e8dc98569e3f04
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2020-10-14 17:50:41 -0700

    Hack some signin/signoff and active commands

--- nelliecat/__main__.py
+++ nelliecat/__main__.py
@@ -1,6 +1,7 @@
 import os
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 
@@ -8,10 +9,43 @@ load_dotenv()
 TOKEN = os.getenv("TOKEN")
 CLIENT = discord.Client()
 
+BOT = commands.Bot(command_prefix="$")
 
-@CLIENT.event
-async def on_ready():
-    """Let us know that the bot is combat ready"""
-    print("NellieCat online and ready")
+ACTIVE_CONSULTANT = None
 
-CLIENT.run(TOKEN)
+
+def has_role(name, roles):
+    role_names = [r.name for r in roles]
+    if name in role_names:
+        return True
+    else:
+        return False
+
+
+@BOT.command(name="signin")
+async def signin(ctx):
+    member = ctx.author
+    if has_role("Consultant", member.roles):
+        global ACTIVE_CONSULTANT
+        ACTIVE_CONSULTANT = member
+        await ctx.send(f"{member.nick} signed in as active consultant.")
+    else:
+        await ctx.send("Sorry, that command is reserved for consultants!")
+
+
+@BOT.command(name="active")
+async def active(ctx):
+    if ACTIVE_CONSULTANT:
+        await ctx.send(f"{ACTIVE_CONSULTANT.nick} is currently in!")
+    else:
+        await ctx.send(f"No consultant currently signed in at the moment.")
+
+@BOT.command(name="signoff")
+async def signoff(ctx):
+    global  ACTIVE_CONSULTANT
+    if ACTIVE_CONSULTANT == ctx.author:
+        ACTIVE_CONSULTANT = None
+        await ctx.send(f"Bye {ctx.author.nick}!")
+
+
+BOT.run(TOKEN)
